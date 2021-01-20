0649375f800659f21f20ffa1033a98611a4ef9d8
Vic Flores <vicsito2014@gmail.com>
2021-01-20 11:13:37 -0600

    Aplicacion convertida a PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -53,6 +54,14 @@ module.exports = {
 
     new MiniCssExtractPlugin({
       filename: 'assets/[name].css',
+    }),
+
+    new CopyPlugin({
+      patterns: [
+        { from: 'public/manifest.json', to: '' },
+        { from: 'public/service-worker.js', to: '' },
+        { from: 'public/icon.png', to: 'assets' },
+      ],
     }),
   ],
 
